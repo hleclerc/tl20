a5cca1851d9d75617ae039f1ca7ca6a61f5abaaf
Hugo Leclerc <hugal.leclerc@gmail.com>
2024-10-07 13:44:10 +0200

    dbg

--- src/cpp/tl/support/containers/CtRange.h
+++ src/cpp/tl/support/containers/CtRange.h
@@ -8,7 +8,11 @@ BEG_TL_NAMESPACE
 template<int beg,int end>
 struct CtRange {
     static void for_each_item( auto &&f ) {
-        if constexpr ( beg < end ) {
+        if constexpr ( end - beg == 3 ) {
+            f( CtInt<beg+0>() );
+            f( CtInt<beg+1>() );
+            f( CtInt<beg+2>() );
+        } else if constexpr ( beg < end ) {
             f( CtInt<beg>() );
             CtRange<beg + 1, end>::for_each_item( FORWARD( f ) );
         }
