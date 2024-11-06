d63207e2bd13d4f29ae7f7fa213b3b5a320cdfd0
Hugo Leclerc <hugal.leclerc@gmail.com>
2024-11-06 15:46:52 +0100

    cstspan

--- src/cpp/tl/support/containers/CstSpan.h
+++ src/cpp/tl/support/containers/CstSpan.h
@@ -1,15 +1,15 @@
 #pragma once
 
-#include "../common_types.h"
-#include "CtType.h"
-#include "CtInt.h"
+#include "Span.h"
 
 BEG_TL_NAMESPACE
 
 /// ct known size version
 template<class T,int size_=-1>
-struct Span {
-    constexpr          Span      ( T *data ) : _data( data ) {}
+struct CstSpan {
+    T_i constexpr      CstSpan   ( CstSpan<T,i> data ) : _data( data.data() ) {}
+    T_i constexpr      CstSpan   ( Span<T,i> data ) : _data( data.data() ) {}
+    constexpr          CstSpan   ( const T *data ) : _data( data ) {}
 
     constexpr auto     size      () const { return CtInt<size_>(); }
 
@@ -25,18 +25,19 @@ struct Span {
     const T*           end       () const { return _data + size_; }
     T*                 end       () { return _data + size_; }
 
-    auto               subspan   ( PI offset ) const { return Span<T>( _data + offset, size_ - offset ); }
+    auto               subspan   ( PI offset ) const { return CstSpan<T>( _data + offset, size_ - offset ); }
 
 private:
-    T*                 _data;
+    const T*           _data;
 };
 
 /// dynamic version
 template<class T>
-struct Span<T,-1> {
-    constexpr          Span      ( T *data, PI size ) : _data( data ), _size( size ) {}
-    T_i constexpr      Span      ( Span<T,i> data ) : _data( data.data() ), _size( data.size() ) {}
-    constexpr          Span      () : _data( nullptr ), _size( 0 ) {}
+struct CstSpan<T,-1> {
+    constexpr          CstSpan   ( const T *data, PI size ) : _data( data ), _size( size ) {}
+    T_i constexpr      CstSpan   ( CstSpan<T,i> data ) : _data( data.data() ), _size( data.size() ) {}
+    T_i constexpr      CstSpan   ( Span<T,i> data ) : _data( data.data() ), _size( data.size() ) {}
+    constexpr          CstSpan   () : _data( nullptr ), _size( 0 ) {}
 
     constexpr auto     size      () const { return _size; }
 
@@ -52,25 +53,25 @@ struct Span<T,-1> {
     const T*           end       () const { return _data + _size; }
     T*                 end       () { return _data + _size; }
 
-    auto               subspan   ( PI beg, PI end ) const { return Span<T>( _data + beg, end - beg ); }
-    auto               subspan   ( PI beg ) const { return Span<T>( _data + beg, _size - beg ); }
+    auto               subspan   ( PI beg, PI end ) const { return CstSpan<T>( _data + beg, end - beg ); }
+    auto               subspan   ( PI beg ) const { return CstSpan<T>( _data + beg, _size - beg ); }
 
     bool               empty     () const { return _size == 0; }
 
     void               resize    ( PI size ) { _size = size; }
 
 private:
-    T*                 _data;
+    const T*           _data;
     PI                 _size;
 };
 
 // common functions
 #define DTP template<class T,int s>
-#define UTP Span<T,s>
+#define UTP CstSpan<T,s>
 
-DTP auto get_compilation_flags( auto &cn, CtType<UTP> ) { cn.add_inc_file( "tl/containers/Span.h" ); }
+DTP auto get_compilation_flags( auto &cn, CtType<UTP> ) { cn.add_inc_file( "tl/containers/CstSpan.h" ); }
 DTP void for_each_template_arg( CtType<UTP>, auto &&f ) { f( CtType<T>() ); f( CtInt<s>() ); }
-DTP auto template_type_name( CtType<UTP> ) { return "Span"; }
+DTP auto template_type_name( CtType<UTP> ) { return "CstSpan"; }
 
 #undef DTP
 #undef UTP
