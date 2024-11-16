8cf9dc4fc46201bc40146a4357897d2cae67c741
Hugo Leclerc <hugal.leclerc@gmail.com>
2024-11-16 15:26:47 +0100

    ctr

--- src/cpp/tl/support/containers/CtRange.h
+++ src/cpp/tl/support/containers/CtRange.h
@@ -8,28 +8,23 @@ BEG_TL_NAMESPACE
 template<int beg,int end>
 struct CtRange {
     static void for_each_item( auto &&f ) {
-        if constexpr ( end - beg == 3 ) {
-            f( CtInt<beg+0>() );
-            f( CtInt<beg+1>() );
-            f( CtInt<beg+2>() );
-        } else if constexpr ( beg < end ) {
-            f( CtInt<beg>() );
-            CtRange<beg + 1, end>::for_each_item( FORWARD( f ) );
-        }
+        f( CtInt<beg>() );
+        CtRange<beg + 1, end>::for_each_item( FORWARD( f ) );
     }
 
     static bool find_item( auto &&f ) {
-        if constexpr ( end - beg == 3 ) {
-            if ( f( CtInt<beg+0>() ) ) return true;
-            if ( f( CtInt<beg+1>() ) ) return true;
-            if ( f( CtInt<beg+2>() ) ) return true;
-            return false;
-        } else if constexpr ( beg < end ) {
-            if ( f( CtInt<beg>() ) )
-                return true;
-            return CtRange<beg + 1, end>::find_item( FORWARD( f ) );
-        }
-        // hum
+        if ( f( CtInt<beg>() ) )
+            return true;
+        return CtRange<beg + 1, end>::find_item( FORWARD( f ) );
+    }
+};
+
+template<int end>
+struct CtRange<end,end> {
+    static void for_each_item( auto &&f ) {
+    }
+
+    static bool find_item( auto &&f ) {
         return false;
     }
 };
