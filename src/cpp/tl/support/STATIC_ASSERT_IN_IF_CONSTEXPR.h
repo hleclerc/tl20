af70371a152daf6646dd7015b82f2c6a29b5c78e
Hugo Leclerc <hugal.leclerc@gmail.com>
2024-11-19 11:32:52 +0100

    g++ < 14

--- src/cpp/tl/support/STATIC_ASSERT_IN_IF_CONSTEXPR.h
+++ src/cpp/tl/support/STATIC_ASSERT_IN_IF_CONSTEXPR.h
@@ -1,4 +1,9 @@
 #pragma once
 
-#define STATIC_ASSERT_WITH_RETURN_IN_IF_CONSTEXPR( RET, COND, MSG ) do { static_assert( COND, MSG ); return RET; } while( false )
-#define STATIC_ASSERT_IN_IF_CONSTEXPR( COND, MSG ) static_assert( COND, MSG )
+#ifdef SDOT_DEBUG_MODE
+    #define STATIC_ASSERT_WITH_RETURN_IN_IF_CONSTEXPR( RET, COND, MSG ) do { static_assert( COND, MSG ); return RET; } while( false )
+    #define STATIC_ASSERT_IN_IF_CONSTEXPR( COND, MSG ) static_assert( COND, MSG )
+#else
+    #define STATIC_ASSERT_WITH_RETURN_IN_IF_CONSTEXPR( RET, COND, MSG ) return RET
+    #define STATIC_ASSERT_IN_IF_CONSTEXPR( COND, MSG ) 
+#endif
