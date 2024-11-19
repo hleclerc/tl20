649cb5f3ce7dcc7a0d8f79ce1e7ff0c68e87f8be
Hugo Leclerc <hugal.leclerc@gmail.com>
2024-11-19 17:00:06 +0100

    static assert

--- src/cpp/tl/support/STATIC_ASSERT_IN_IF_CONSTEXPR.h
+++ src/cpp/tl/support/STATIC_ASSERT_IN_IF_CONSTEXPR.h
@@ -1,4 +1,11 @@
 #pragma once
 
+#ifdef TL_STATIC_ASSERT
 #define STATIC_ASSERT_WITH_RETURN_IN_IF_CONSTEXPR( RET, COND, MSG ) do { static_assert( COND, MSG ); return RET; } while( false )
 #define STATIC_ASSERT_IN_IF_CONSTEXPR( COND, MSG ) static_assert( COND, MSG )
+#else
+#include <assert.h>
+
+#define STATIC_ASSERT_WITH_RETURN_IN_IF_CONSTEXPR( RET, COND, MSG ) do { assert( COND ); return RET; } while( false )
+#define STATIC_ASSERT_IN_IF_CONSTEXPR( COND, MSG ) assert( COND )
+#endif
