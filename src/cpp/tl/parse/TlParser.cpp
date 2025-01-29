5a7b959a76d2a62113e8d971b46cd980270d9489
Hugo Leclerc <hugal.leclerc@gmail.com>
2025-01-29 16:35:29 +0100

    touching

--- src/cpp/tl/parse/TlParser.cpp
+++ src/cpp/tl/parse/TlParser.cpp
@@ -1,12 +1,9 @@
 #include "../support/string/va_string.h"
-#include "../support/ASSERT.h"
 #include "../support/ERROR.h"
-#include "../support/TODO.h"
-#include "../support/P.h"
-#include "tl/parse/OperatorTrie.h"
+
 #include "tl/parse/OperatorTrie_Tl.h"
 #include "tl/parse/TlToken.h"
-#include <limits>
+
 #include "TlParser.h"
 
 BEG_TL_NAMESPACE
@@ -15,10 +12,13 @@ BEG_TL_NAMESPACE
 inline bool letter( int c ) { return ( c >= 'a' && c <= 'z' ) || ( c >= 'A' && c <= 'Z' ) || c == '_'; }
 inline bool number( int c ) { return c >= '0' && c <= '9'; }
 inline bool space ( int c ) { return c == ' ' || c == '\t' || c == '\r'; }
-inline bool oper  ( int c ) { return c == '+' || c == '~' || c == '-' || c == '|' || c == '\\' || c == '^' || 
-                                     c == '@' || c == '=' || c == '+' || c == '"' || c == '$'  || c == '%' || 
-                                     c == '*' || c == '!' || c == ':' || c == '/' || c == '.'  || c == '?' || 
-                                     c == '<' || c == '>' || c == '"' || c == '`' || c == '\'' ; }
+
+inline bool oper  ( int c ) {
+     return c == '+' || c == '~' || c == '-' || c == '|' || c == '\\' || c == '^' || 
+            c == '@' || c == '=' || c == '+' || c == '"' || c == '$'  || c == '%' || 
+            c == '*' || c == '!' || c == ':' || c == '/' || c == '.'  || c == '?' || 
+            c == '<' || c == '>' || c == '"' || c == '`' || c == '\'' ;
+}
 
 inline bool alphan( int c ) { return letter( c ) || number( c ); }
 
@@ -32,8 +32,7 @@ inline bool is_cnt_for_number( int c, int prev_char_value ) {
     return alphan( c );
 }
 
-TlParser::TlParser() {
-    operator_trie = OperatorTrie_Tl::default_tl_operator_trie();
+TlParser::TlParser() : local_operator_trie( false ) {
     _init();
 }
 
@@ -45,16 +44,24 @@ void TlParser::display( Displayer &ds ) const {
 }
 
 void TlParser::_init() {
+    // operator_trie
+    if ( local_operator_trie ) {
+        local_operator_trie = false;
+        delete operator_trie;
+    }
+    operator_trie = OperatorTrie_Tl::default_tl_operator_trie();
+
+    // pending_...
+    prev_token_is_touching = false;
+    pending_new_line = true;
+    pending_comma = false;
+
+    // other attributes
     curr_tok_src_refs.clear();
     curr_tok_content.clear();
-    restart_jump = nullptr;
-
-    just_seen_a_semicolon = false;
-    just_seen_a_new_line = false;
-    just_seen_a_comma = false;
-    just_seen_a_space = false;
+    prev_char_value = -1;
     prev_line_beg.clear();
-
+    restart_jump = nullptr;
     token_stack.clear();
     pool.clear();
 }
@@ -73,9 +80,9 @@ void TlParser::_parse( int c, const char *nxt, const char *beg, const char *end,
         if ( oper  ( c ) ) goto beg_operator;
         if ( c == '\n'   ) goto beg_new_line;
         if ( c == '#'    ) goto beg_comment;
-        if ( c == '('    ) { _on_opening_paren( TlToken::Type::ParenthesisCall, ')' ); goto inc_switch; }
-        if ( c == '['    ) { _on_opening_paren( TlToken::Type::BracketCall    , ']' ); goto inc_switch; }
-        if ( c == '{'    ) { _on_opening_paren( TlToken::Type::BraceCall      , '}' ); goto inc_switch; }
+        if ( c == '('    ) { _on_opening_paren( TlToken::Type::ParenthesisCall, "operator ()", ')' ); goto inc_switch; }
+        if ( c == '['    ) { _on_opening_paren( TlToken::Type::BracketCall    , "operator []", ']' ); goto inc_switch; }
+        if ( c == '{'    ) { _on_opening_paren( TlToken::Type::BraceCall      , "operator {}", '}' ); goto inc_switch; }
         if ( c == ')'    ) { _on_closing_paren( ')' ); goto inc_switch; }
         if ( c == ']'    ) { _on_closing_paren( ']' ); goto inc_switch; }
         if ( c == '}'    ) { _on_closing_paren( '}' ); goto inc_switch; }
@@ -184,12 +191,29 @@ void TlParser::_parse( int c, const char *nxt, const char *beg, const char *end,
     #undef PARSE_TYPE
 }
 
-void TlParser::_on_opening_paren( TlToken::Type call_type, char expected_closing ) {
+void TlParser::_on_opening_paren( TlToken::Type call_type, const char *func_name, char expected_closing ) {
+    if ( prev_token_is_touching ) { // `some_code( a, b )` -> `a` and `b` will be the children of `some_code`
+        TlToken *token_func = _new_token( TlToken::Type::ParenthesisCall );
+        _take_token( token_func, { .left_prio = operator_trie->prio_call } );
+        token_stack.back().closing_char = expected_closing;
+    } else { // `some_code ( a, b ), c` -> `( a, b )` and `c` will be the children of `some_code`
+        TlToken *token_name = _new_token( TlToken::Type::Variable, func_name );
+        TlToken *token_func = _new_token( TlToken::Type::ParenthesisCall );
+        token_func->add_child( token_func );
+        _append_token( token_func, {} );
+    }
 
+    prev_token_is_touching = false;
+    pending_new_line = false;
+    pending_comma = false;
 }
 
 void TlParser::_on_closing_paren( char c ) {
+    TODO;
 
+    prev_token_is_touching = false;
+    pending_new_line = false;
+    pending_comma = false;
 }
 
 void TlParser::_on_semicolon() {
@@ -201,36 +225,25 @@ void TlParser::_on_semicolon() {
     if ( token_stack.back().closing_char == 0 )
         _pop_stack_item();
 
-    //
-    just_seen_a_semicolon = true;
-    just_seen_a_new_line = false;
-    just_seen_a_space = false;
-    just_seen_a_comma = false;
+    prev_token_is_touching = false;
+    pending_new_line = false;
+    pending_comma = false;
 }
 
 void TlParser::_on_comma() {
-    if ( just_seen_a_semicolon )
-        return _error( "a comma just after a semicolon does not have a defined behavior." );
-    if ( just_seen_a_new_line )
-        return _error( "a comma at the beginning of a new line does not have a defined behavior." );
-    if ( just_seen_a_comma )
+    prev_token_is_touching = false;
+
+    if ( pending_new_line ) {
+        _update_stack_after_newline();
+        pending_new_line = false;
+    }
+
+    if ( pending_comma ) 
         return _error( "a comma just after another comma does not have a defined behavior." );
-
-    // things like `(,`
-    if ( token_stack.back().closing_char )
-        return _error( "impossible to go higher" );
-
-    // gp up
-    _pop_stack_item();
-
-    //
-    just_seen_a_semicolon = false;
-    just_seen_a_new_line = false;
-    just_seen_a_space = false;
-    just_seen_a_comma = true;
+    pending_comma = true;
 }
 
-void TlParser::_update_stack_after_nl() {
+void TlParser::_update_stack_after_newline() {
     while ( token_stack.size() ) {
         const StackItem &si = token_stack.back();
         if ( si.closing_char ) {
@@ -243,6 +256,12 @@ void TlParser::_update_stack_after_nl() {
     }
 }
 
+void TlParser::_update_stack_after_comma() {
+    const StackItem &si = token_stack.back();
+    if ( si.closing_char == 0 && si.on_a_new_line == false )
+        _pop_stack_item();
+}
+    
 TlToken *TlParser::_new_token( TlToken::Type type, StrView content ) {
     TlToken *token = pool.create<TlToken>();
 
@@ -311,12 +330,19 @@ void TlParser::_on_new_line() {
     }
 
     prev_line_beg = curr_tok_content;
-    just_seen_a_new_line = true;
+
+    prev_token_is_touching = false;
+    pending_new_line = true;
+    pending_comma = false;
 }
 
 void TlParser::_on_variable() {
     TlToken *tok = _new_token( TlToken::Type::Variable, curr_tok_content );
     _append_token( tok, { .max_nb_children = 0 } );
+
+    prev_token_is_touching = false;
+    pending_new_line = false;
+    pending_comma = false;
 }
 
 void TlParser::_on_operator() {
@@ -350,6 +376,10 @@ void TlParser::_on_operator() {
 
         use_operator( od, tok_call );
 
+        prev_token_is_touching = false;
+        pending_new_line = false;
+        pending_comma = false;
+
         curr_tok_src_refs.back().beg += od->str.size();
         str.remove_prefix( od->str.size() );
     }
@@ -366,10 +396,14 @@ void TlParser::_on_number() {
 
     // seen as a variable
     _append_token( tok_call, { .max_nb_children = 0 } );
+
+    prev_token_is_touching = false;
+    pending_new_line = false;
+    pending_comma = false;
 }
 
 void TlParser::_on_space() {
-    just_seen_a_space = true;
+    prev_token_is_touching = false;
 }
 
 bool TlParser::_take_token( TlToken *token, const TakingInfo &ti ) {
@@ -384,9 +418,6 @@ bool TlParser::_take_token( TlToken *token, const TakingInfo &ti ) {
     si.token = token;
 
     // update stack item
-    si.newline_size = int( prev_line_beg.size() );
-    si.on_a_new_line = just_seen_a_new_line;
-
     si.max_nb_children = ti.max_nb_children;
     si.closing_char = 0;
     si.prio = ti.right_prio;
@@ -396,8 +427,10 @@ bool TlParser::_take_token( TlToken *token, const TakingInfo &ti ) {
 
 void TlParser::_append_token( TlToken *token, const AppendingInfo &pti ) {
     // update the stack
-    if ( just_seen_a_new_line )
-        _update_stack_after_nl();
+    if ( pending_new_line )
+        _update_stack_after_newline();
+    else if ( pending_comma )
+        _update_stack_after_comma();
 
     // make a call token if last stack item does not take children
     if ( token_stack.back().max_nb_children == 0 ) {
@@ -418,17 +451,11 @@ void TlParser::_append_token( TlToken *token, const AppendingInfo &pti ) {
     token_stack << StackItem{
         .token = token,
         .closing_char = 0,
-        .on_a_new_line = just_seen_a_new_line,
+        .on_a_new_line = pending_new_line,
         .newline_size = int( prev_line_beg.size() ),
         .max_nb_children = pti.max_nb_children,
         .prio = pti.right_prio
     };
-
-    //
-    just_seen_a_semicolon = false;
-    just_seen_a_new_line = false;
-    just_seen_a_comma = false;
-    just_seen_a_space = false;
 }
 
 END_TL_NAMESPACE
--- src/cpp/tl/parse/TlParser.h
+++ src/cpp/tl/parse/TlParser.h
@@ -18,53 +18,55 @@ BEG_TL_NAMESPACE
 */
 class TlParser {
 public:
-    /**/             TlParser               ();
+    /**/             TlParser                   ();
+      
+    void             parse                      ( StrView content, PI src_off, AstWriterStr src_url );
+    void             dump                       ( AstWriter &writer );
+    
+    void             display                    ( Displayer &ds ) const;
+                     
+private:      
+    struct           AppendingInfo              { int max_nb_children = std::numeric_limits<int>::max(); int right_prio = 100; };
+    struct           TakingInfo                 { int max_nb_children = std::numeric_limits<int>::max(); int right_prio = 100, left_prio = 100; };
+    using            StackItem                  = TlParserStackItem;
+    using            SrcRef                     = TlToken::SrcRef;
+      
+    enum {           eof                        = -1 }; ///< internal codes for _parse
+                     
+    void             _on_opening_paren          ( TlToken::Type call_type, const char *func_name, char expected_closing );
+    void             _on_closing_paren          ( char c );
+    void             _on_semicolon              ();
+    void             _on_new_line               ();
+    void             _on_variable               ();
+    void             _on_operator               ();
+    void             _on_number                 ();
+    void             _on_space                  ();
+    void             _on_comma                  ();
   
-    void             display                ( Displayer &ds ) const;
-    void             parse                  ( StrView content, PI src_off, AstWriterStr src_url );
-    void             dump                   ( AstWriter &writer );
-                 
-private:  
-    struct           AppendingInfo          { int max_nb_children = std::numeric_limits<int>::max(); int right_prio = 100; };
-    struct           TakingInfo             { int max_nb_children = std::numeric_limits<int>::max(); int right_prio = 100, left_prio = 100; };
-    using            StackItem              = TlParserStackItem;
-    using            SrcRef                 = TlToken::SrcRef;
-  
-    enum {           eof                    = -1 }; ///< internal codes for _parse
-                 
-    void             _on_opening_paren      ( TlToken::Type call_type, char expected_closing );
-    void             _on_closing_paren      ( char c );
-    void             _on_semicolon          ();
-    void             _on_new_line           ();
-    void             _on_variable           ();
-    void             _on_operator           ();
-    void             _on_number             ();
-    void             _on_space              ();
-    void             _on_comma              ();
-  
-    void             _update_stack_after_nl (); ///< update stack position after a new line
-    void             _pop_stack_item        ();
-    void             _append_token          ( TlToken *token, const AppendingInfo &ai );
-    bool             _take_token            ( TlToken *token, const TakingInfo &ti );
-    TlToken*         _new_token             ( TlToken::Type type, StrView content = {} );
-    void             _error                 ( Str msg, Vec<TlToken *> tok = {} );
-    void             _parse                 ( int c, const char *nxt, const char *beg, const char *end, AstWriterStr src_url );
-    void             _init                  ();
-        
-    Vec<SrcRef>      curr_tok_src_refs;     ///<
-    Str              curr_tok_content;      ///<
-    int              prev_char_value;       ///< used notably for cnt_number where '+' or '-' can be continuing chars if preceded by 'e' or 'E'
-    void*            restart_jump;          ///< where to go at the beginning of `parse`
- 
-    bool             just_seen_a_semicolon; ///<
-    bool             just_seen_a_new_line;  ///<
-    bool             just_seen_a_comma;     ///<
-    bool             just_seen_a_space;     ///<
-    Str              prev_line_beg;         ///<
-  
-    OperatorTrie_Tl* operator_trie;         ///<
-    Vec<StackItem>   token_stack;           ///<
-    BumpPointerPool  pool;                  ///<
+    void             _update_stack_after_newline(); ///< update stack position after a new line
+    void             _update_stack_after_comma  (); ///< update stack position after a comma
+    void             _pop_stack_item            ();
+    void             _append_token              ( TlToken *token, const AppendingInfo &ai );
+    bool             _take_token                ( TlToken *token, const TakingInfo &ti );
+    TlToken*         _new_token                 ( TlToken::Type type, StrView content = {} );
+    void             _error                     ( Str msg, Vec<TlToken *> tok = {} );
+    void             _parse                     ( int c, const char *nxt, const char *beg, const char *end, AstWriterStr src_url );
+    void             _init                      ();
+    
+    bool             local_operator_trie;       ///<
+    OperatorTrie_Tl* operator_trie;             ///<
+    
+    bool             prev_token_is_touching;    ///<
+    bool             pending_new_line;          ///<
+    bool             pending_comma;             ///<
+    
+    Vec<SrcRef>      curr_tok_src_refs;         ///<
+    Str              curr_tok_content;          ///<
+    int              prev_char_value;           ///< used notably for cnt_number where '+' or '-' can be continuing chars if preceded by 'e' or 'E'
+    Str              prev_line_beg;             ///<
+    void*            restart_jump;              ///< where to go at the beginning of `parse`
+    Vec<StackItem>   token_stack;               ///<
+    BumpPointerPool  pool;                      ///<
 };
 
 END_TL_NAMESPACE
